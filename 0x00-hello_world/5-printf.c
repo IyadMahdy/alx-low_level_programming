3ff111e695382aab396d677681c265cca6d4fafb
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-16 09:25:40 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point of program
+ * Return 0 if succesful and any other number otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+}
