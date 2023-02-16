9eea1c01eb8eff30a5ffa5530369e52d8affa5f3
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-16 10:20:57 -0800

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Starting Point
+ * return: 0 if successful and anything else otherwise
+ */
+
+int main(void)
+{
+	printf("Size of char: %d byte(s)", sizeof(char));
+	printf("Size of char: %d byte(s)", sizeof(int));
+	printf("Size of char: %d byte(s)", sizeof(long int));
+	printf("Size of char: %d byte(s)", sizeof(long long int));
+	printf("Size of char: %d byte(s)", sizeof(float));
+	return (0);
+}
