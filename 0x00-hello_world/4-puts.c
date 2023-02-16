0befa5669b3324c3bd68f7d5f7752b24219f4294
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-16 09:22:32 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - Starting-point of the program
+ * Return: 0 if the program ended succesfully. Any other number otherwise
+ */
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
