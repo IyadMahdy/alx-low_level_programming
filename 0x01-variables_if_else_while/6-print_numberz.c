841be6740601b291907e91b18151c83edcb9a6a1
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-18 04:49:07 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Start
+ * Return: 0 if successful, others otherwise
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar('0' + num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
