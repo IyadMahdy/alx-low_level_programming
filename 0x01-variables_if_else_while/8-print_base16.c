4f812698872f2756f0096b36ce2cb8991d14e703
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-18 04:57:31 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Starting Point
+ * Return: 0 if successful and other otherwise
+ */
+int main(void)
+{
+	int n = 0; 
+	char c = 'a';
+
+	while (n <= 9)
+	{
+		putchar('0' + n);
+		n++
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
