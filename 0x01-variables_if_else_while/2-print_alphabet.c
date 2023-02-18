06ba42ac84272628b585527226556041222f7290
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-18 03:59:53 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Starting Point
+ * Return: 0 if successful and other otherwise
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
