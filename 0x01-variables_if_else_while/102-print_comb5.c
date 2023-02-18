4695a30736e78114f33c81e3c0c424455ef5bccb
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-18 07:47:12 -0800

    Advanced Task 2

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - Start
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int num1, num2, num3 = 0, num4;
+
+	while (num3 <= 9)
+	{
+		num4 = 0;
+		while (num4 <= 9)
+		{
+			num1 = 0;
+			while (num1 <= 9)
+			{
+				num2 = 0;
+				while (num2 <= 9)
+				{
+					if (num1 != 0 || num2 != 0)
+					{
+						putchar('0' + num3);
+						putchar('0' + num4);
+						putchar(' ');
+						putchar('0' + num1);
+						putchar('0' + num2);
+						if (num1 != 9 || num2 != 9 || num3 != 9 || num4 != 9)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+					num2++;
+				}
+				num1++;
+			}
+			num4++;
+		}
+		num3++;
+	}
+	putchar('\n');
+
+	return (0);
+}
