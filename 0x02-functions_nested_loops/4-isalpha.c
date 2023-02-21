0d1df860abab1b3f7d9411ad9c691092a5a0af5d
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-21 05:19:09 -0800

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,7 +10,9 @@ int _isalpha(int c)
 {
 	char character = c;
 
-	if ((character >= 97 && character <= 122) || (character >= 65 && character <= 90))
+	if (character >= 97 && character <= 122)
+		return (1);
+	else if (character >= 65 && character <= 90)
 		return (1);
 	else
 		return (0);
