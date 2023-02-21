849960202b5d8609a398ea10bcb3378dc13089a4
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-21 10:30:35 -0800

    Task 7

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int hours1, hours2, minutes1, minutes2;
+
+	hours1 = 0;
+	while (hours1 <= 2)
+	{
+		hours2 = 0;
+		while (hours2 <= 9)
+		{
+			minutes1 = 0;
+			while (minutes1 <= 5)
+			{
+				minutes2 = 0;
+				while (minutes2 <= 9)
+				{
+					if (hours1 == 2 && hours2 == 4)
+						return;
+					_putchar(hours1);
+					_putchar(hours2);
+					_putchar(':');
+					_putchar(minutes1);
+					_putchar(minutes2);
+					_putchar('\n');
+					minutes2++;
+				}
+				minutes1++;
+			}
+			hours2++;	
+		}
+		hours1++;
+	}
+}
