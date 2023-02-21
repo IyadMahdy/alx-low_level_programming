e8c7b6316ad15257a63977f61e52f7dc787b4e51
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-21 05:30:09 -0800

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints sign of number
+ * @n: number
+ *
+ * Return: 1 if positive, 0 if 0, -1 if negative
+ */
+int print_signt(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0)
+	}
+}
