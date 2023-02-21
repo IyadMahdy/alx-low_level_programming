b51e79b9f6ef8d2d4e2b47e87b5ae540b0afa09c
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-21 14:44:13 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/** add - Adds two numbers
+ * @n1: first number
+ * @n2: second number
+ *
+ * Return: Addition of two numbers
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table,
+ * starting with zero
+ */
+void times_table(void)
+{
+	
+}
