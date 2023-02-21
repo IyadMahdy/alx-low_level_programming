4f476521ecbb69bae066fdf2841b75b92117e9a1
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-21 02:03:15 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Print Alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return (0);
+}
