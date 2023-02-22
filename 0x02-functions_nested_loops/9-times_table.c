5929c44218c5002fdb02adad24b61c0bb1eb8687
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-22 05:00:06 -0800

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,5 +6,44 @@
  */
 void times_table(void)
 {
-	
+	int i, j, num1, num2;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			num1 = (i * j) / 10;
+			num2 = (i * j) % 10;
+
+			if (j == 0)
+			{
+				if (num1 != 0)
+					_putchar('0' + num1);
+				_putchar('0' + num2);
+				_putchar(',');
+			}
+
+			else if (j == 9)
+			{
+				_putchar(' ');
+				if ((i * j) < 10)
+					_putchar(' ');
+				if (num1 != 0)
+					_putchar('0' + num1);
+				_putchar('0' + num2);
+			}
+
+			else
+			{
+				_putchar(' ');
+				if ((i * j) < 10)
+					_putchar(' ');
+				if (num1 != 0)
+					_putchar('0' + num1);
+				_putchar('0' + num2);
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
 }
