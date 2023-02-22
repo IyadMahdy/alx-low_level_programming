43c2cc03b461ce1cf498940d609892d664ab1e55
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-22 07:55:57 -0800

    Advanced Task 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,22 +1,33 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times table,
+ * print_times_table - Prints the @n times table,
  * starting with zero
+ * @n: number to print times table of
  */
 void print_times_table(int n)
 {
-	int i, j, num1, num2;
+	int i, j, num1, num2, num3;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
+		return;
+	}
 
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
+			num3 = (i * j) / 100
 			num1 = (i * j) / 10;
 			num2 = (i * j) % 10;
 
 			if (j == 0)
 			{
+				if (num3 != 0)
+					_putchar('0' + num3);
 				if (num1 != 0)
 					_putchar('0' + num1);
 				_putchar('0' + num2);
@@ -25,9 +36,14 @@ void print_times_table(int n)
 
 			else if (j == n)
 			{
-				_putchar(' ');
-				if ((i * j) < 10)
+				if ((i * j) < 100)
+				{	
 					_putchar(' ');
+					if ((i * j) < 10)
+						_putchar(' ');
+				}
+				if (num3 != 0)
+					_putchar('0' + num3);
 				if (num1 != 0)
 					_putchar('0' + num1);
 				_putchar('0' + num2);
@@ -35,9 +51,14 @@ void print_times_table(int n)
 
 			else
 			{
-				_putchar(' ');
-				if ((i * j) < 10)
+				if ((i * j) < 100)
+				{
 					_putchar(' ');
+					if ((i * j) < 10)
+						_putchar(' ');
+				}
+				if (num3 != 0)
+					_putchar('0' + num3);
 				if (num1 != 0)
 					_putchar('0' + num1);
 				_putchar('0' + num2);
