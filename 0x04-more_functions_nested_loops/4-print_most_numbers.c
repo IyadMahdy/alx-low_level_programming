c47f887e26abd181ff02c8b48c9529a78e575609
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-24 10:06:51 -0800

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_most_functions - print from 0 to 9 except 2 and 4
@@ -12,8 +11,8 @@ void print_most_functions(void)
 	while (c <= '9')
 	{
 		if (!(c == 2 || c == 4))
-			putchar(c);
+			_putchar(c);
 		c++;
 	}
-	putchar('\n');
+	_putchar('\n');
 }
