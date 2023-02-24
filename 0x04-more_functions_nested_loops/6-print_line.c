cee968fe88921c11416a1fcdbcd8c0ae8220a8d2
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-24 10:49:01 -0800

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,11 +8,14 @@ void print_line(int n)
 {
 	int count;
 
-	count = 0;
-	while (count <= n)
+	if (n > 0)
 	{
-		_putchar('_');
-		count++;
+		count = 0;
+		while (count <= n)
+		{
+			_putchar('_');
+			count++;
+		}
 	}
 	_putchar('\n');
 }
