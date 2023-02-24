d2f7acb152f2fffadbdf23aa679681d8a826be1b
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-24 10:46:36 -0800

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: number of times the character _ should be printed
+ */
+void print_line(int n)
+{
+	int count;
+
+	count = 0;
+	while (count <= n)
+	{
+		_putchar('_');
+		count++;
+	}
+}
