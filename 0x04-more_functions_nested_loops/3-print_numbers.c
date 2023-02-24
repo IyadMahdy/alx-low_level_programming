31239ce372f97efcb45ef52d988937349a22bef4
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-24 09:14:48 -0800

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_numbers - Prints from 0 to 9
@@ -7,9 +8,11 @@ void print_numbers(void)
 {
 	char c;
 
+	c = '0';
 	while (c <= '9')
 	{
-		_putchar(c);
+		putchar(c);
+		c++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
