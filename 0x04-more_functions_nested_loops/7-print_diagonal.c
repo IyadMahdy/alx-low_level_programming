6ccc77b2603c72d8f06accd38a7e9fef6c06a866
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-24 13:24:56 -0800

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal
+ * @n: number of times to print \
+ */
+void print_diagonal(int n)
+{
+	int count, i;
+
+	count = 0;
+	if (n > 0)
+	{
+		while (count < n)
+		{
+			for (i = 0; i <= count; i++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+			count++;
+		}
+	}
+	else
+		_putchar('\n');
+}
