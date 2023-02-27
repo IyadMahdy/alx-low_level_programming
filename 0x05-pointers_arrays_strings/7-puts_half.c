b0fc1e74f8105b9901fe0d3d775dd258b66e6fbb
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-27 09:32:32 -0800

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half a string
+ * @str: Pointer to first char
+ */
+void puts_half(char *str)
+{
+	int n, len = 0;
+
+	while (str[len])
+		len++;
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+	for (; n < len; n++)
+		_putchar(str[n])
+	_putchar('\n');
+}
