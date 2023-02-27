4d4f128ad8c98a833e06a402597f14ee1ce555cc
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-02-27 08:16:52 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by new line to stdout
+ * @str: Pointer to first character of string
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str)
+	}
+	_putchar('\n');
+}
