04d9245359786841694a549dac4ff98cdd2b019f
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-03-08 04:15:47 -0800

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns length of string
+ *
+ * @s: Ponter to first char of string
+ * Return: Length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+	{
+		return 1 + _strlen_recursion (s + 1);
+	}
+}
