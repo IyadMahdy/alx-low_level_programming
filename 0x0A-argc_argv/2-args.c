aeb7a7b64eefe8ccaef450224b63a8e0d60a0174
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-03-10 05:51:59 -0800

    Task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,9 +9,12 @@
  */
 int main(int argc, char *argv[])
 {
-	for (int i = 0; i < argc; i++)
+	if (argc > 0)
 	{
-		printf("%s\n", argv[i]);
+		for (int i = 0; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
 	}
 
 	return (0);
