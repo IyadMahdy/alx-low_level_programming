6c5ee07cc5c8c7504b2f58d8f37b10ffbb98f344
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-03-10 06:11:18 -0800

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.c>
+
+/**
+ * main - Adds positive numbers from arguments
+ *
+ * @argc: Argument Count
+ * @argv: Arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum, count;
+
+	sum = 0;
+	count = 1;
+	while (argv[count])
+	{
+		if (atoi(argv[count]))
+			sum += atoi(argv[count]);
+		else
+		{
+			printf("Error\n");
+			return (0);
+		}
+		count++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
