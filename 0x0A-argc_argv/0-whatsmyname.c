4ddc9b165d9ac83426f94630c265940f22c7917a
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-03-10 05:39:07 -0800

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,12 @@
  * main - Prints Name of Program
  *
  * @argc: Argument Count
- * @argv: Argument Vector
+ * @argv: Arguments
  * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
 
 	return (0);
