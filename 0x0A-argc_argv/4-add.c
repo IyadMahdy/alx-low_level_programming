89d9d0fc38ba2fec24503601147d83167deefc64
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-03-10 06:28:40 -0800

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,37 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
- * main - Adds positive numbers from arguments
- *
- * @argc: Argument Count
- * @argv: Arguments
- * Return: Always 0 except if non digit in argument
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
  */
+
 int main(int argc, char *argv[])
 {
-	int sum, count;
+	int i, j, length, sum;
+	char *ptr;
 
 	if (argc < 2)
-	{
 		printf("0\n");
-		return (0);
-	}
-	sum = 0;
-	count = 1;
-	while (count < argc)
+	else
 	{
-		if (atoi(argv[count]))
-			sum += atoi(argv[count]);
-		else
+		sum = 0;
+		for (i = 1; i < argc; i++)
 		{
-			printf("Error\n");
-			return (1);
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
 		}
-		count++;
-	}
 
 	printf("%d\n", sum);
-
+	}
 	return (0);
 }
