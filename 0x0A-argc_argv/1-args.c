2af3c8c928e3a309d77e4a47e5c767c32b62c95f
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-03-10 05:44:36 -0800

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints Number of Arguments
+ *
+ * @argc: Argument Count
+ * @argv: Arguments
+ * Return: Always 0;
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
