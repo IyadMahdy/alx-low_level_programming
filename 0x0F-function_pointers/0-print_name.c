64c08afb15aad5ee2cf8ab90bedee4535489c6a2
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-03-23 06:19:08 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
-#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "function_pointers.h"
 
 /**
  * print_name - Prints a name
@@ -10,7 +10,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+        if (name == NULL || f == NULL)
+                return;
+        f(name);
 }
