054b62cab4078c333e607762f09b3f2ce92ce6d7
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-03-31 11:42:15 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,11 @@
 #include "lists.h"
+
 /**
  * print_list - a function that prints all the elements of a list_t list.
  * @h: singly linked list
  *
  * Return: the number of nodes
  */
-
 size_t print_list(const list_t *h)
 {
 	unsigned int i = 0;
