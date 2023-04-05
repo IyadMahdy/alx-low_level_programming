497fff1ce50d6c80e9209deb787d58a0fcc87353
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-04-05 11:43:12 -0700

    Task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,9 +15,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (node == NULL)
 		return (NULL);
 
+	while (head->next != NULL)
+		head = head->next;
 	node->n = n;
 	node->next = NULL;
 
-	*head = node;
+	head->next = node;
 	return (node);
 }
