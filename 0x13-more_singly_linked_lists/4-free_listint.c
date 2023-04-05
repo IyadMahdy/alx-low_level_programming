35374be106c69d98989e7b1d0d2f1a52f3d62b4d
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-04-05 12:15:34 -0700

    Task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,9 +9,9 @@ void free_listint(listint_t *head)
 {
 	listint_t *ptr;
 
-	while (*head != NULL)
+	while (head != NULL)
 	{
-		ptr = *head;
+		ptr = head;
 		while (ptr->next != NULL)
 			ptr = ptr->next;
 		free(ptr);
