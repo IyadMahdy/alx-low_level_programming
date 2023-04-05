f3d3d08d7a8f81c84123991eef42afea7c315c31
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-04-05 11:31:07 -0700

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,10 +10,15 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t node = malloc(sizeof(listint_t));
+	listint_t *node = malloc(sizeof(listint_t));
+
 	if (node == NULL)
 		return (NULL);
 
-	node->next = &head;
+	node->next = *head;
 	node->n = n;
+
+	*head = node;
+
+	return (node);
 }
