650c65fa5aabe82f87aa8fb6fc896fd9021db36b
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-04-05 12:48:08 -0700

    Task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,6 +16,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 	ptr = head;
 	while (index--)
+	{
 		ptr = ptr->next;
+		if (ptr = NULL)
+			return (NULL);
+	}
 	return (ptr);
 }
