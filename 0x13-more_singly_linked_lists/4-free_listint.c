a4373e74aa1aa76bfacb94a195515a0459305f98
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-04-05 12:13:36 -0700

    Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list.
+ *
+ * @head: pointer to list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while (*head != NULL)
+	{
+		ptr = *head;
+		while (ptr->next != NULL)
+			ptr = ptr->next;
+		free(ptr);
+	}
+}
