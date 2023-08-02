96119300c16bbb4da30783aaaa2445145fe54887
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-08-02 07:34:43 -0700

    Update Header File

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,20 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
