68d4a2ba5e8a9daba469448cd5942cb67afb5ab1
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-08-02 09:09:34 -0700

    Update Advanced Tast 1

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,6 +15,8 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (s1[0] == '\0' && s2[1] != '\0')
 			return (0);
+		else if (s1[0] == '\0' && s2[1] == '\0')
+			return (1);
 		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
 			return (1);
 	}
