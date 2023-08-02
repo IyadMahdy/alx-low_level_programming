30ac0406140d21e8037aed3c2f7452f964807751
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-08-02 07:38:32 -0700

    Update Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _puts_rev_recursion - Prints String in reverse with Recursion
+ * _print_rev_recursion - Prints String in reverse with Recursion
  * @s: String to be printed
  *
  * Return: Nothing
  */
-void _puts_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (s[0] == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_puts_rev_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar(s[0]);
 }
