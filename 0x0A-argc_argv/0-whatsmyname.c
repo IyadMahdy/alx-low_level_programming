eb59b1d6739c5873d7d1824afcf17481da16a152
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-08-04 05:13:57 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Prints the number of arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc)
+		;
+	printf("%s\n", argv[0]);
+	return (0);
+}
