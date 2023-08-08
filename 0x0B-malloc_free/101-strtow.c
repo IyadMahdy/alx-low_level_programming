3e73739b5f115e616cc8729f113748630d05add1
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-08-08 05:29:34 -0700

    Advanced Task 1

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,78 @@
+#include "main.h"
+
+/**
+ * word_count - Gets Number of Words
+ * @str: String
+ *
+ * Return: Number of Words
+ */
+int word_count(char *str)
+{
+	int i = 0, word = 0;
+
+	while (str[i])
+	{
+		while (str[i] == 32)
+			i++;
+		if (!str[i])
+			break;
+		word++;
+	}
+	return (word);
+}
+
+/**
+ * strtow - split a string into words
+ * @str: a pointer to the string to split
+ *
+ * Return: NULL if memory allocation fails or if str is NULL or empty (""),
+ * otherwise return a pointer to the array of words terminated by a NULL
+ */
+char **strtow(char *str);
+{
+	char **arr;
+	char *w;
+	int i, j, words, c, count;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	words = word_count(str);
+	if (!words)
+		return (NULL);
+
+	arr = (char **) malloc((words + 1) * sizeof(char *));
+
+	if (!arr)
+		return (NULL);
+	for (i = 0, j = 0; str[i]; i++)
+	{
+		if (str[i] == 32)
+			i++;
+		else
+		{
+			w = str;
+			c = 0;
+			count = 0;
+			for (; str[i] != 32 && str[i]; i++)
+				c++;
+			arr[j] = malloc((c + 1) * sizeof(char));
+			if (!arr[j])
+			{
+				for (; j > 0;)
+					free(arr[--j]);
+				free(arr);
+				return (NULL);
+			}
+			while (*w != 32 && *w)
+			{
+				arr[j][count] = *w;
+				w++;
+				count++;
+			}
+			arr[j][count] = '\0';
+			j++;
+		}
+	}
+	return (arr);
+}
