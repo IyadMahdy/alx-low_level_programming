b1ba21f8bc722c6fc1a5b9532b67ffd54280579d
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-08-14 02:48:14 -0700

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Prints the file name of the program
+  *
+  * Return: Always zero
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
