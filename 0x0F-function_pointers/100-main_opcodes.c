df00e326cc74beeb48f573f311a478c3d7da7da8
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-08-16 09:43:48 -0700

    Advanced Task 0

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Main
+ * @argc: ...
+ * @argv: ...
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num_bytes, i;
+	char *opcode = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < num_bytes - 1; i++)
+	{
+		printf("%x ", opcode[i] & 0xff);
+	}
+	printf("%x\n", opcode[i] & 0xff);
+
+	return (0);
+}
