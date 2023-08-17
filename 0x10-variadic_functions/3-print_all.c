012fb3e8c1793333bbd41ee5904ad9f55eb8dede
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-08-17 12:40:06 -0700

    Task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - Prints all
+ * @format: Format
+ */
+void print_all(const char * const format, ...)
+{
+	va_list = args;
+	char *sep = ", "
+	int i;
+	char *str;
+
+	while (format[i] && format)
+	{
+		if (!format[i + 1])
+			sep = "\n";
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c%s", va_arg(args, int), sep);
+				break;
+			case 'i':
+				printf("%d%s", va_arg(args, int), sep);
+				break;
+			case 'f':
+				printf("%f%s", va_arg(args, double), sep);
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", str, sep);
+				break;
+		}
+		i++;
+	}
+	va_end(args);
+}
