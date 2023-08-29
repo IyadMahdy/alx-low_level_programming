3720744e7546b85633503dc8ea01e0d0bf277067
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-08-29 07:19:55 -0700

    Another Fix Advanced Task 2

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,37 +1,6 @@
 #include "lists.h"
 
 /**
- * loop_finder - Finds a loop in a list
- * @head: Head of list
- *
- * Return: Node where loop occurs
- */
-listint_t *loop_finder(listint_t *head)
-{
-	listint_t *p, *q;
-
-	if (!head)
-		return (NULL);
-	p = head;
-	q = head->next;
-	while (q)
-	{
-		if (q == q->next)
-			return (q);
-
-		while (p != q)
-		{
-			if (q->next == p)
-				return (q);
-			p = p->next;
-		}
-		p = head;
-		q = q->next;
-	}
-	return (NULL);
-}
-
-/**
  * free_listint_safe - Frees a listint_t list safely
  * @h: Pointer to head of list
  *
@@ -56,6 +25,8 @@ size_t free_listint_safe(listint_t **h)
 		free(ptr);
 		i++;
 	}
+
+	h = NULL;
 
 	return (i);
 }
