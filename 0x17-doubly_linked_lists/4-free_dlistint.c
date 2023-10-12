f40348b6931f6725ee5909e1292fa30514ba4cca
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-10-12 03:23:45 -0700

    Task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees doubly-linked list
+ * @head: Head of list
+ *
+ * Return: Nothing
+ */
+void free_dlistint(dlistint_d *head)
+{
+	dlistint_d *ptr;
+
+	ptr = head->next;
+
+	while (ptr)
+	{
+		free(head);
+		head = ptr;
+		ptr = ptr->next;
+	}
+	free(head);
+}
