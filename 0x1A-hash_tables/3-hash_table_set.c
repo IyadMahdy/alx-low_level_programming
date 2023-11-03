09149dd297c3f56c88f4564c99763bc2d070d956
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-11-03 07:43:12 -0700

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,47 +12,40 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_element;
 	unsigned long int size, index;
+	char *key2, *value2;
 
 	if (!ht || !key || !key[0] || !(ht->size) || !ht->array)
 		return (0);
 
 	size = ht->size;
+	key2 = strdup(key);
+	if (!key2)
+		return (0);
+	value2 = strdup(value);
+	if (!value2)
+		return (free(key2), 0);
+
 	new_element = calloc(1, sizeof(hash_node_t));
 	if (!new_element)
-		return (0);
-	new_element->key = strdup(key);
-	if (!(new_element->key))
-	{
-		free(new_element);
-		return (0);
-	}
-	new_element->value = strdup(value);
-	if (!(new_element->value))
-	{
-		free(new_element->key);
-		free(new_element);
-		return (0);
-	}
+		return (free(key2), free(value2), 0);
+
+	new_element->key = key2;
+	new_element->value = value2;
 
 	index = key_index((unsigned char *)key, size);
 
 	if (strcmp(ht->array[index]->key, key) == 0)
 	{
 		free(ht->array[index]->value);
-		ht->array[index]->value = strdup(new_element->value);
-		free(new_element->key);
-		free(new_element->value);
+		ht->array[index]->value = value2;
+		free(key2);
 		free(new_element);
 	}
 	else
 	{
-		if (!ht->array[index])
-			ht->array[index] = new_element;
-		else
-		{
-			new_element->next = ht->array[index];
-			ht->array[index] = new_element;
-		}
+		new_element->next = ht->array[index];
+		ht->array[index] = new_element;
 	}
+
 	return (1);
 }
