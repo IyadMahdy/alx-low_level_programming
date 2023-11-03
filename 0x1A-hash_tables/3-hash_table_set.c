136c8010da5d197f83cbc4e9b6a3c219bbc64596
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-11-03 06:51:42 -0700

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -39,7 +39,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (strcmp(ht->array[index]->key, key) == 0)
 	{
 		free(ht->array[index]->value);
-		ht->array[index]->value = new_element->value;
+		ht->array[index]->value = strdup(new_element->value);
 		free(new_element->key);
 		free(new_element);
 	}
