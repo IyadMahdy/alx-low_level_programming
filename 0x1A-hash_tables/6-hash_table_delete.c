1c13428df2d5caf27677d68a939cb2d96abe0ea6
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-11-03 09:44:29 -0700

    Task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: Hash Table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *ptr;
+	unsigned long int i;
+
+	if (!ht || !ht->size)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		ptr = ht->array[i];
+		while (ht->array[i])
+		{
+			ptr = ht->array[i]->next;
+			free(ht->array[i]);
+			ht->array[i] = ptr;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
