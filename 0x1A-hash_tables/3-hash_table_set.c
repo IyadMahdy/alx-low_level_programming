43645eb29c2f792c4ca593fe7314a92510b09afe
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-11-03 06:39:01 -0700

    Fix Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,10 +13,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_element;
 	unsigned long int size, index;
 
-	size = ht->size;
-	if (!ht || !key || !key[0] || !size || !ht->array)
+	if (!ht || !key || !key[0] || !ht->size || !ht->array)
 		return (0);
 
+	size = ht->size;
 	new_element = malloc(sizeof(hash_node_t));
 	if (!new_element)
 		return (0);
